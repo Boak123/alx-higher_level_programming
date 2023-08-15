d3adb4a043d25c5a448cdee9e4fac4adc89312bb
root <boak123>
2023-08-15 08:52:13 -0700

    update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # 102-magic_calculation.py
 
+
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+    """Match bytecode provided by alx."""
     from magic_calculation_102 import add, sub
 
     if a < b:

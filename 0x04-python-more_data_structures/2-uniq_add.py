2912a4f439779003d67e45a189509824714672aa
root <boak123>
2023-08-16 03:26:20 -0700

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 2-uniq_add.py
+
+def uniq_add(my_list=[]):
+    """adds all unique integers in a list."""
+    result = 0
+    for x in set(my_list):
+        return += x
+    return (result)
+

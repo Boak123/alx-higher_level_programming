3488ec45547e5fc1d4551e3cb4e6a4e158305873
root <boak123>
2023-08-30 13:18:28 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Define a class square."""
+"""Define a class Square."""
 
 
-class square:
-    """Represents a square."""
+class Square:
+    """Represent a square."""
     pass

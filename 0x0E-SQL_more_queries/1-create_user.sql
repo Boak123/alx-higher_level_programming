f1083d573e3ac1124baacb93962623ab7f87c028
boak123 <boluwatifeakorede17@gmail.com>
2023-10-18 17:20:50 +0000

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost';
     IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES
+GRANT ALL PRIVILEGES
     ON *.*
     TO 'user_0d_1'@'localhost';
     IDENTIFIED BY ''user_0d_1_pwd';

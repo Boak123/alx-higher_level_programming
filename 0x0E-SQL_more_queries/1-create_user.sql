51bbd6410ba021eb76b1f6b5d9b00452c9c7cf17
boak123 <boluwatifeakorede17@gmail.com>
2023-10-18 17:07:33 +0000

    update

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+--  creates the MySQL server user user_0d_1.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhostt';
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANTS ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
+    IDENTIFIED BY ''user_0d_1_pwd';
+FLUSH PRIVILEGES 

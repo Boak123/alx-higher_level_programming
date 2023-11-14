f1c47cbc89ca68beeb677d07d2c989733dac4ae6
boak123 <boluwatifeakorede17@gmail.com>
2023-11-13 20:40:15 -0800

    updated

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// function that executes x times a function.
+// executes x times a function.
 
 exports.callMeMoby = function (x, theFunction) {
-	for (let i = 0; i < x; i++) {
-		theFunction();
-	}
-}
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};

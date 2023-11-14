da797e0664f13d851042316ebf468c317518e7c2
boak123 <boluwatifeakorede17@gmail.com>
2023-11-13 20:47:02 -0800

    updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-// script that prints a message depending of the number of arguments passed
+// Prints a message depending of the number of arguments passed
 
-if (process.argv.length === 2)
-{
-	console.log('No argument');
-}
-else if (process.argv.length === 3)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Argument founds');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }

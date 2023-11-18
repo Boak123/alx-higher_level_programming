d8e7411549cb73c181c120ae73e8ff816dcf60da
boak123 <boluwatifeakorede17@gmail.com>
2023-11-18 05:19:46 -0800

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """  lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
-if __name__ = "__main__"
-    db = MySQLbd.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+if __name__ == "__main__"
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur db.cursor
     cursor.execute("""SELECT * FROM states WHERE 'N%' ORDER BY states.id"")
     row = cur.fetchall()

a1a0d038abc65157dd6888d874af007fc052720c
boak123 <boluwatifeakorede17@gmail.com>
2023-12-01 14:03:15 -0800

    updated

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""displays the body of the response
+"""
+
+if __name__ == "__main__":
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    from sys import argv
+
+    value = {"email": argv[2]}
+    request = Request(
+            argv[1], urlencode(value).encode("ascii"))
+    with urlopen(request) as response:
+        head = response.headers.get('X-Request-Id')
+        print(response.read().decode('utf-8'))
